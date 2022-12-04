e180b53764a83f2ffde11e41604017dc441fdd85
Stephen Buckley <stephenbuckley@Stephens-MacBook-Pro.local>
2022-12-04 22:30:44 +0000

    day 4 part 1 complete

---
+++ day4.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+
+const day4 = () => {
+  const data = fs.readFileSync("./day4Data.txt", "utf-8");
+  const array = data.split("\n");
+  let split = [];
+  let crossOver = 0;
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    const firstSplit = element.split(",");
+    const secondSplit = firstSplit.map((value) => {
+      return value.split("-");
+    });
+    split.push(secondSplit);
+  }
+  for (let i = 0; i < split.length; i++) {
+    const elfPair = split[i];
+    const firstElf = elfPair[0];
+    const secondElf = elfPair[1];
+    if (
+      parseInt(firstElf[0]) <= parseInt(secondElf[0]) &&
+      parseInt(firstElf[1]) >= parseInt(secondElf[1])
+    ) {
+      crossOver++;
+    } else if (
+      parseInt(firstElf[0]) >= parseInt(secondElf[0]) &&
+      parseInt(firstElf[1]) <= parseInt(secondElf[1])
+    ) {
+      crossOver++;
+    }
+  }
+  console.log(crossOver);
+};
+
+day4();
